c3d7bf4b03a1bc84864faab544fe1dcc816d0284
Osmel02 <157715359+Osmel02@users.noreply.github.com>
2025-01-24 20:47:12 -0500

    Actualizando Codigos

---
+++ Practica/Listas.py
@@ -0,0 +1,20 @@
+lista = [3,3,4,5,6]
+lista2 =lista.copy()
+print(lista2)
+if lista == lista2 : print('Son iguales')
+
+tupla = tuple(lista)
+conjunto = set(lista)
+print(tupla)
+print(conjunto)
+conjunto2 = {2,3,3,4,5,44}
+print(conjunto2)
+
+diccionario = {
+    1: 'Hop',
+    2: 'Cash',
+    3: 'Exit'
+}
+print(diccionario.values())
+string = str("Mi nombre es Osmel")
+print(len(string))
